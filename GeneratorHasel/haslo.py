b20fb7fdc5881dafdc3066bda871ea61635dd107
wuefi <wuefiak.op.pl>
2026-04-03 23:00:57 +0200

    Usuniecie cyfr z generatora hasel

--- GeneratorHasel/haslo.py
+++ GeneratorHasel/haslo.py
@@ -2,10 +2,10 @@
 import string
 
 def generuj_haslo(dlugosc=12):
-    znaki = string.ascii_letters + string.digits + string.punctuation
+    znaki = string.ascii_letters + string.punctuation
     haslo = ''.join(random.choice(znaki) for i in range(dlugosc))
     return haslo
 
-print('--- GENERATOR HASIEL ---')
+print('--- GENERATOR HASEL ---')
 dl = int(input('Jak dlugie ma byc haslo? '))
 print(f'Twoje bezpieczne haslo to: {generuj_haslo(dl)}')
