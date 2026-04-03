e786ec8bf1bb0485a67fc0f97f21dd176b7e3d69
wuefi <wuefiak.op.pl>
2026-04-03 23:52:58 +0200

    Pelna interakcja: uzytkownik decyduje o liczbie i dlugosci hasel

--- GeneratorHasel/haslo.py
+++ GeneratorHasel/haslo.py
@@ -1,12 +1,16 @@
 ﻿import random
 import string
 
-def generuj_haslo(dlugosc=12):
-    znaki = string.ascii_letters + string.punctuation
+def generuj_haslo(dlugosc=20):
+    znaki = string.ascii_letters + string.digits + string.punctuation
     haslo = ''.join(random.choice(znaki) for i in range(dlugosc))
     return haslo
 
-print('--- GENERATOR 5 HASIEL ---')
+print('--- GENERATOR HASEL ---')
 dl = int(input('Jak dlugie ma byc haslo? '))
-for i in range(5):
+ile = int(input('Ile hasel chcesz wygenerowac? '))
+
+print(f' --- Twoje bezpieczne hasla o dlugosci {dl} ---')
+for i in range(ile):
     print(f'{i+1}. {generuj_haslo(dl)}')
+print(f"Wygenerowano pomyslnie {ile} hasel o dlugości {dl} znakow")
