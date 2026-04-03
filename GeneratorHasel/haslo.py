eaf358477982a921dfac150e4ee9df2ff42ce4d5
wuefi <wuefiak.op.pl>
2026-04-03 22:52:06 +0200

    Program do generowania hasel

---
+++ GeneratorHasel/haslo.py
@@ -0,0 +1,11 @@
+﻿import random
+import string
+
+def generuj_haslo(dlugosc=12):
+    znaki = string.ascii_letters + string.digits + string.punctuation
+    haslo = ''.join(random.choice(znaki) for i in range(dlugosc))
+    return haslo
+
+print('--- GENERATOR HASIEL ---')
+dl = int(input('Jak dlugie ma byc haslo? '))
+print(f'Twoje bezpieczne haslo to: {generuj_haslo(dl)}')
